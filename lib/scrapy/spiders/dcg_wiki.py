fc7d373ae0535a1058d9db3f53db49ea2697f57a
Jeff Ruberg <rubergly@gmail.com>
2025-11-22 19:39:53 -0800

    parse errata data into json

--- lib/scrapy/spiders/dcg_wiki.py
+++ lib/scrapy/spiders/dcg_wiki.py
@@ -19,7 +19,12 @@ CARD_LIST_PATHS = [
     "/wiki/Category:Promo",
 ]
 
-DEBUG_CARD = 'EX10-074'
+KNOWN_ERRATA_WITHOUT_TABLES = [
+    'https://digimoncardgame.fandom.com/wiki/BT6-084/Errata',  # Sistermon Ciel
+]
+
+# DEBUG_CARD = 'EX10-074'
+DEBUG_CARD = 'EX9-021'
 DEBUG_ON = True
 
 
@@ -185,7 +190,7 @@ class DCGWikiSpider(base_spider.BaseSpider):
       thumb_img = item.css('.thumb img.thumbimage')
       img_name = thumb_img.xpath('@data-image-key').get()
       thumb_url = item.css('.thumb img.thumbimage').xpath('@src').get()
-      full_url = re.sub(r'/scale-to-width-down/\d+', '', thumb_url)
+      full_url = self.full_img_for_thumb(thumb_url)
       caption_text = thumb_img.xpath('@alt').get()
       caption_link = item.css('.lightbox-caption a::attr(href)').get()
       if 'cb=' in full_url:
@@ -194,15 +199,20 @@ class DCGWikiSpider(base_spider.BaseSpider):
 
       imgs.append({
           'img_name': img_name,
-          'full_url': full_url,
+          'img_url': full_url,
           'file_page': wiki_url(wiki_file_path),
           'upload_date': upload_date,
           'caption_text': caption_text,
           'caption_link': wiki_url(caption_link)
       })
 
+    # FIXME: store this in a main json file instead
     yield JSONItem(data={'images': imgs},
                    subpath=[set_id, card_num, 'gallery_images.json'])
+
+  @staticmethod
+  def full_img_for_thumb(thumb_url: str) -> str:
+    return re.sub(r'/scale-to-width-down/\d+', '', thumb_url)
 
   def parse_card_rulings(self, response):
     logging.debug('GET %s | %s', response.status, response.url)
@@ -227,11 +237,35 @@ class DCGWikiSpider(base_spider.BaseSpider):
     card_num = meta.get('card_num')
     set_id = meta.get('set_id')
 
-    body = response.css('.page__main .mw-body-content').get()
-    text = "\n".join([
-        "<html>",
-        body,
-        "</html>",
-    ])
+    tables = response.css('.errata-table')
 
-    yield TextItem(data=text, subpath=[set_id, card_num, 'errata.html'])
+    if len(tables) == 0:
+      # we only expect this for a couple exceptional pages.
+      # these pages are weird enough that we can just ignore them.
+      if response.url in KNOWN_ERRATA_WITHOUT_TABLES:
+        return
+
+      assert False, f"no errata tables found for {response.url}"
+
+    data = []
+
+    for table in tables:
+      before_text = ''.join(table.css('.before-text ::text').getall())
+      after_text = ''.join(table.css('.after-text ::text').getall())
+
+      before_thumb_url = table.css('.before-image img').xpath('@src').get()
+      after_thumb_url = table.css('.after-image img').xpath('@src').get()
+
+      item = {
+          'before_text': before_text,
+          'after_text': after_text,
+          'after_image_url': self.full_img_for_thumb(after_thumb_url),
+      }
+
+      if before_thumb_url:
+        item['before_image_url'] = self.full_img_for_thumb(before_thumb_url)
+
+      data.append(item)
+
+    # FIXME: store this in a main json file instead
+    yield JSONItem(data=data, subpath=[set_id, card_num, 'errata.json'])
